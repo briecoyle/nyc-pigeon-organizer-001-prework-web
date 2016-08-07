c2ced416850e433e5e3a507ec11ebd2b22784ac9
Brie Coyle <brie.coyle@gmail.com>
2016-08-07 22:45:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeons = {}
+  data.each do |attribute, info|
+    info.each do |detail, names|
+      names.each do |name|
+        organized_pigeons[name] ||= {}
+        organized_pigeons[name][attribute] ||= []
+        organized_pigeons[name][attribute] << detail.to_s
+      end
+    end
+  end
+  organized_pigeons
 end
